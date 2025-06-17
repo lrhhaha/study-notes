9c04e9d0905322806fe6a40441db770102dce29c
lrh <910584399@qq.com>
2025-06-17 17:17:45 +0800

    编写扁平化函数

--- frontend/手写JS/flat.js
+++ frontend/手写JS/flat.js
@@ -1,20 +1,30 @@
-// 支持自定义扁平化深度控制
-// 处理空数组和undefined元素
 
-function flat(array, deep) {
+// 使用reduce实现指定深度的数组扁平化函数
+function flattenArrayWithDepth(arr, depth = 1) {
+  return depth > 0 ? arr.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      return acc.concat(flattenArrayWithDepth(item, depth - 1));
+    } else {
+      return acc.concat(item);
+    }
+  }, []) : arr.slice();
+}
+
+const arr = [1,2, [3, [4]]]
+console.log(flattenArrayWithDepth(arr, 1))
+
+function flat(array, depth) {
   if (!Array.isArray(array)) {
     throw new TypeError("arguments[0] must be an array");
   }
 
-  deep = deep ?? Infinity;
+  depth = depth ?? Infinity;
 
   return array.reduce((acc, curr) => {
-    if (deep > 0 && Array.isArray(curr)){
-      return acc.concat(flat(curr, deep - 1))
+    if (depth > 0 && Array.isArray(curr)){
+      return acc.concat(flat(curr, depth - 1))
     } else {
       return [...acc, curr]
     }
   }, [])
 }
-
-console.log(flat([1, [2, 3], [4, [5, 6]]], 1));
